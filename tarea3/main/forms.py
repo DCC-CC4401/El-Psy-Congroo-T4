cc4d7ac03fced100649cdc4a315d29a4b3412b3f
Bastianleaf <bastian.martinez@ing.uchile.cl>
2017-05-24 21:10:05 -0400

    Agregado formulario para gestion de productos

--- tarea3/main/forms.py
+++ tarea3/main/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from .models import UsuarioAlumno
+from .models import UsuarioAlumno, Comida
+
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = UsuarioAlumno
         fields = ('id','nombre', 'email','tipo','avatar','contraseña',)
+
+class GestionProductosForm(forms.ModelForm):
+
+    class Meta:
+        model = Comida
+        fields = ('idVendedor','nombre','categorias','descripcion','stock','precio','imagen')
