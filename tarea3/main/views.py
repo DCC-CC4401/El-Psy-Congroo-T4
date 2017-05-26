531e42b67bed9d726db1cdd491b6793043e04d8b
aleql <harambedcc@gmail.com>
2017-05-25 20:33:38 -0400

    En el formulario de creacion de usuario se arreglo el campo formas de pago Se arreglo la creacion de sesion

--- tarea3/main/views.py
+++ tarea3/main/views.py
@@ -24,23 +24,27 @@ def loggedin(request):
 
 def loginReq(request):
     tipo = 0
+    url = ''
+    id = 0
     email = request.POST.get("email")
     password = request.POST.get("password")
     for p in Usuario.objects.raw('SELECT * FROM usuario'):
         if p.contraseña == password and p.email == email:
-            print(p.id)
             tipo=p.tipo
             if (tipo==0):
-                return render(request, 'main/baseAdmin.html', {})
-            if (tipo == 1):
-                return render(request, 'main/baseAlumno.html', {})
-            if (tipo == 2):
-                return render(request, 'main/baseVFijo.html', {})
-            if (tipo == 3):
-                return render(request, 'main/baseVAmbulante.html', {})
-
-
-    #return render(request, 'main/loggedin.html', {"email" : tipo})
+                url= 'main/baseAdmin.html'
+            elif (tipo == 1):
+                url = 'main/baseAlumno.html'
+            elif (tipo == 2):
+                url = 'main/baseVFijo.html'
+            elif (tipo == 3):
+                url= 'main/baseVAmbulante.html'
+        id = p.id
+        tipo = p.tipo
+    request.session['id'] = id
+    request.session['tipo'] = tipo
+    request.session['email'] = email
+    return render(request, url, {"email" : email, "tipo" : tipo, "id": id})
 
 def gestionproductos(request):
     return render(request, 'main/gestion-productos.html', {})
@@ -76,11 +80,18 @@ def register(request):
     tipo = request.POST.get("tipo")
     nombre = request.POST.get("nombre")
     email = request.POST.get("email")
-    #group1 = request.POST.get("group1")
     password = request.POST.get("password")
     horaInicial = request.POST.get("horaIni")
     horaFinal = request.POST.get("horaFin")
-    formasDePago = request.POST.get("formasDePago[]")
+    formasDePago =[]
+    if not (request.POST.get("formaDePago0") is None):
+        formasDePago.append(request.POST.get("formaDePago0"))
+    if not (request.POST.get("formaDePago1") is None):
+        formasDePago.append(request.POST.get("formaDePago1"))
+    if not (request.POST.get("formaDePago2") is None):
+        formasDePago.append(request.POST.get("formaDePago2"))
+    if not (request.POST.get("formaDePago3") is None):
+        formasDePago.append(request.POST.get("formaDePago3"))
     usuarioNuevo = Usuario(nombre=nombre,email=email,tipo=tipo,contraseña=password,avatar="jpge",formasDePago=formasDePago,horarioIni=horaInicial,horarioFin=horaFinal)
     usuarioNuevo.save()
     return render(request, 'main/loggedin.html', {"email" : email})
